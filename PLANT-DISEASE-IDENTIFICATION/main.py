0912762177cf1ae977daa35b42d7b771733d1826
Rahul Patil <rgpatil9307@gmail.com>
2025-12-12 02:43:56 +0530

    Update main.py

--- PLANT-DISEASE-IDENTIFICATION/main.py
+++ PLANT-DISEASE-IDENTIFICATION/main.py
@@ -155,12 +155,12 @@ elif page == "DISEASE RECOGNITION":
         with open(temp_path, "wb") as f:
             f.write(uploaded.getbuffer())
 
-        if st.button("🔍 Detect Disease"):
+       if st.button("🔍 Detect Disease"):
 
             if model is None:
                 st.error("❌ Model not loaded!")
             else:
-                
+                st.info("⏳ Processing... Please wait")
                 idx, disease, conf = predict_image(temp_path)
 
                 st.success(f"🌱 Predicted Disease: **{disease}**")
@@ -174,6 +174,7 @@ st.markdown("""
 Developed by <b>Team Agri🌾Next</b> | Powered by Streamlit
 </div>
 """, unsafe_allow_html=True)
+
 
 
 
